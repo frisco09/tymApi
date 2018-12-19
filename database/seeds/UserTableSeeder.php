8e2ed8ee77ea84c8876871d5125f8463be67f59c
frisco09 <43142372+frisco09@users.noreply.github.com>
2018-12-19 13:00:01 -0300

    Update UserTableSeeder.php

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -21,10 +21,11 @@ class UserTableSeeder extends Seeder
         $user->name = 'User';
         $user->email = 'user@example.com';
         $user->user_psp = 'userPsp1';
-        $user->phone = '0203456';
+        $user->phone = '02203456';
         $user->status = "no disponible";
         $user->credito = '0';
         $user->verified = '0';
+        $user->user_img_pr = 'user.jgp';
         $user->password = bcrypt('secret');
         $user->save();
         $user->roles()->attach($role_user);
@@ -33,10 +34,11 @@ class UserTableSeeder extends Seeder
         $user1->name = 'User';
         $user1->email = 'user_2@example.com';
         $user1->user_psp = 'userPsp2';
-        $user1->phone = '0203456';
+        $user1->phone = '02303456';
         $user1->status = "no disponible";
         $user1->credito = '0';
         $user1->verified = '0';
+        $user1->user_img_pr = 'user.jgp';
         $user1->password = bcrypt('secret');
         $user1->save();
         $user1->roles()->attach($role_user);
@@ -45,10 +47,11 @@ class UserTableSeeder extends Seeder
         $user2->name = 'User';
         $user2->email = 'user_3@example.com';
         $user2->user_psp = 'userPsp3';
-        $user2->phone = '0203456';
+        $user2->phone = '02403456';
         $user2->status = "no disponible";
         $user2->credito = '0';
         $user2->verified = '0';
+        $user2->user_img_pr = 'user.jgp';
         $user2->password = bcrypt('secret');
         $user2->save();
         $user2->roles()->attach($role_user);
